386a8289aaab62604985dd7711ea6c11ac0d8376
SteveYoungYc <chen032@e.ntu.edu.sg>
2022-09-27 02:34:53 +0000

    fix: cmd quit

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -34,6 +34,7 @@ static int cmd_c(char *args) {
 
 
 static int cmd_q(char *args) {
+  nemu_state.state = NEMU_QUIT;
   return -1;
 }
 
@@ -110,10 +111,7 @@ void sdb_mainloop() {
     int i;
     for (i = 0; i < NR_CMD; i ++) {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
-        if (cmd_table[i].handler(args) < 0) {
-          nemu_state.state = NEMU_QUIT;
-          return;
-        }
+        if (cmd_table[i].handler(args) < 0) { return; }
         break;
       }
     }
