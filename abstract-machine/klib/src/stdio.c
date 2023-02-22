6eb4839b5eee8bbb3818d7aa3a3492314bcdb579
SteveYoungYc <stm32_yc@sjtu.edu.cn>
2023-02-22 14:27:35 +0800

    add: printf

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,9 +5,6 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char* fmt, ...) {
-  panic("Not implemented");
-}
 
 #define _ADDRESSOF(v) (&(v))
 
@@ -21,6 +18,9 @@ int printf(const char* fmt, ...) {
 #define _va_arg _crt_va_arg
 #define _va_end _crt_va_end
 
+#define STR_OUT 0
+#define UART_OUT 1
+
 int _div(long* n, unsigned base) {
   int __res;
   __res = ((unsigned long)*n) % (unsigned)base;
@@ -30,14 +30,11 @@ int _div(long* n, unsigned base) {
 
 #define do_div(n, base) _div(&n, base)
 
-static inline int isdigit(int ch) {
-  return (ch >= '0') && (ch <= '9');
-}
-
-static char* number(char* str, long num, int base) {
+static int number(char** str_p, long num, int base) {
   char tmp[66];
   const char* digits = "0123456789abcdefghijklmnopqrstuvwxyz";
   int i = 0;
+  char* origin_str = *str_p;
   if (num == 0) {
     tmp[i++] = '0';
   } else {
@@ -45,43 +42,60 @@ static char* number(char* str, long num, int base) {
       tmp[i++] = digits[do_div(num, base)];
     }
   }
-  while (i-- > 0)
-    *str++ = tmp[i];
-  return str;
+  if (origin_str != NULL) {
+    while (i-- > 0) {
+      **str_p = tmp[i];
+      (*str_p)++;
+    }
+  } else {
+    while (i-- > 0)
+      putch(tmp[i]);
+  }
+  return *str_p - origin_str;
 }
 
-int vsprintf(char* out, const char* fmt, va_list args) {
-  int len;
+int fmt_parser(const char* fmt, va_list args, int out_tpye, char* out) {
   unsigned long num;
   int i, base;
-  char* str;
+  const char* str_fmt;
+  char* str_out;
   const char* s;
+  int len = 0;
 
-  for (str = out; *fmt; ++fmt) {
-    if (*fmt != '%') {
-      *str++ = *fmt;
+  str_out = out;
+  str_fmt = fmt;
+  for (str_fmt = fmt; *str_fmt; ++str_fmt) {
+    if (*str_fmt != '%') {
+      len++;
+      if (out_tpye == STR_OUT) {
+        *str_out++ = *str_fmt;
+      }
+      else if (out_tpye == UART_OUT) {
+        putch(*str_fmt);
+      }
       continue;
     }
-    ++fmt;
+    ++str_fmt;
     /* default base */
     base = 10;
 
-    switch (*fmt) {
+    switch (*str_fmt) {
       case 's':
         s = _va_arg(args, char*);
-        len = strlen(s);
-        for (i = 0; i < len; ++i)
-          *str++ = *s++;
-        continue;
-
-      case '%':
-        *str++ = '%';
-        continue;
+        int s_len = strlen(s);
+        len += s_len;
+        for (i = 0; i < s_len; ++i) {
+          if (out_tpye == STR_OUT)
+            *str_out++ = *s++;
+          else if (out_tpye == UART_OUT)
+            putch(*s++);
+        }
+        continue; // for continue
 
         /* integer number formats - set up the flags and "break" */
       case 'o':
         base = 8;
-        break;
+        break;  // switch break
 
       case 'x':
         base = 16;
@@ -93,18 +107,18 @@ int vsprintf(char* out, const char* fmt, va_list args) {
         break;
 
       default:
-        *str++ = '%';
-        if (*fmt)
-          *str++ = *fmt;
-        else
-          --fmt;
         continue;
     }
     num = _va_arg(args, int);
-    str = number(str, num, base);
+    len += number(&str_out, num, base);
   }
-  *str = '\0';
-  return str - out;
+  if (out_tpye == STR_OUT)
+    *str_out = '\0';
+  return len;
+}
+
+int vsprintf(char* out, const char* fmt, va_list args) {
+  return fmt_parser(fmt, args, STR_OUT, out);
 }
 
 int sprintf(char* out, const char* fmt, ...) {
@@ -122,6 +136,15 @@ int snprintf(char* out, size_t n, const char* fmt, ...) {
 
 int vsnprintf(char* out, size_t n, const char* fmt, va_list ap) {
   panic("Not implemented");
+}
+
+int printf(const char* fmt, ...) {
+  va_list arg;
+  int done;
+  va_start(arg, fmt);
+  done = fmt_parser(fmt, arg, UART_OUT, NULL);
+  va_end(arg);
+  return done;
 }
 
 #endif
