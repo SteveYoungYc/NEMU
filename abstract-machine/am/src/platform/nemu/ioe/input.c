ecc7253d02c1dbfc1c0b2ce89704daf88757bc50
SteveYoungYc <stm32_yc@sjtu.edu.cn>
2023-02-24 19:06:47 +0800

    add: keyboard

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,7 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint16_t value = inw(KBD_ADDR);
+  kbd->keydown = ((value & 0x8000) != 0);
+  kbd->keycode = (uint8_t)value;
 }
