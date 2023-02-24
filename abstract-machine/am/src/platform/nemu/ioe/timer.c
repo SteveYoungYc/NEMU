451d4d1ef5b0c287ce64cec5e2f32f2006c57e0f
SteveYoungYc <stm32_yc@sjtu.edu.cn>
2023-02-24 19:06:56 +0800

    add: timer

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,11 +1,16 @@
 #include <am.h>
 #include <nemu.h>
 
+uint64_t boot_time = 0;
+
 void __am_timer_init() {
+  boot_time = inl(RTC_ADDR);
 }
 
-void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+void __am_timer_uptime(AM_TIMER_UPTIME_T* uptime) {
+  uptime->us = inl(RTC_ADDR + 4);
+  uptime->us = uptime->us << 32;
+  uptime->us = (uptime->us | inl(RTC_ADDR)) - boot_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
